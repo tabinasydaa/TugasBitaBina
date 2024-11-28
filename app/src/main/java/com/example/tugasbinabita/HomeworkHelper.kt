8de45253b6f18d03c28fe8d69905cf73ce51e46d
TabhitaTriana <bithhfp@gmail.com>
2024-11-28 15:51:26 +0700

    no 5

---
+++ app/src/main/java/com/example/tugasbinabita/HomeworkHelper.kt
@@ -0,0 +1,57 @@
+package com.example.tugasbinabita
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteException
+import com.example.tugasbinabita.DatabaseContract.HomeworkColumns.Companion.TABLE_NAME
+
+class HomeworkHelper(context: Context) {
+    private var databaseHelper : DatabaseHelper = DatabaseHelper(context)
+    private lateinit var database : SQLiteDatabase
+
+    companion object {
+        private const val DATABASE_TABLE = TABLE_NAME
+        private var INSTANCE : HomeworkHelper? = null
+
+        fun getInstance(context: Context) : HomeworkHelper =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: HomeworkHelper(context)
+            }
+    }
+    @Throws(SQLiteException::class)
+    fun open() {
+        database = databaseHelper.writableDatabase
+    }
+    fun close() {
+        databaseHelper.close()
+
+        if (database.isOpen)
+            database.close()
+    }
+
+    fun queryAll(): Cursor {
+        return database.query(
+            DATABASE_TABLE,
+            null,
+            null,
+            null,
+            null,
+            null,
+            "$_ID ASC",
+            null)
+    }
+
+    fun insert(values: ContentValues?): Long {
+        return database.insert(DATABASE_TABLE, null, values)
+    }
+    fun update(id: String, values: ContentValues?): Int {
+        return database.update(DATABASE_TABLE, values, "$_ID = ?", arrayOf(id))
+    }
+    fun deleteById(id: String): Int {
+        return database.delete(DATABASE_TABLE, "$_ID = '$id'", null)
+    }
+
+
+}
